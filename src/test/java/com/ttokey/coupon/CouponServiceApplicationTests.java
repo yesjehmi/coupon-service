8ba3cb2e394e7c7a29aedd82364e4619fa968a14
이석희 <seokhee.lee2@cj.net>
2023-01-28 01:28:52 +0900

    init project

---
+++ src/test/java/com/ttokey/coupon/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttokey.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
